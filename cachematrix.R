8f1b2e6b80bb62d42fec98da86252a949e5364fb
anton-weigardh <anton.weigardh@gmail.com>
2014-07-22 22:54:41 +0200

    Updates
    Script updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R
+## This script contains two functions, makeCacheMatrix and cacheSolve.
+## A given matrix is cached. Later, the inverse of the matrix is calculated.
+## The functions takes a matrix, x and calculates the inverse of said matrix, x^-1.
+## In order to execute properly, the matrix must be invertible (i.e not singular and of size NxN, N = {2,3,...})
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## In makeCacheMatrix, a "special" matrix is created that can cache the inverse of the matrix.
+makeCacheMatrix <- function(x = matrix()){
+  m <- NULL
+  set <- function(y){
+    x <<- y     
+    m <<- NULL
+  }
+  get <- function() x
+  set_inverse <- function(solve) m <<- solve
+  get_inverse <- function() m
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## In cacheSolve, the inverse of the matrix is computed
+cacheSolve <- function(x, ...){
+  m <- x$get_inverse()
+  if(!is.null(m)){
+    message("Getting cached data...")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$set_inverse(m)
+  m
 }
+
+# Test script with, for example,
+#mat <- matrix(rnorm(9),3)
+#mat2 <- matrix(c(1,1,2,3),2)
+#cacheSolve(makeCacheMatrix(mat))
+#cacheSolve(makeCacheMatrix(mat2))
